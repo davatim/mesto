62cf0475c18b12b52aa813273ae2103fe384fbdf
David Timofeev <dava.tim@yandex.ru>
2023-04-20 00:31:19 +0300

    Исправления связи попап имадж и тайтл

--- src/components/PopupWithImage.js
+++ src/components/PopupWithImage.js
@@ -2,8 +2,12 @@ import Popup from "./Popup.js";
 export default class PopupWithImage extends Popup {
   constructor(popupSelector, image, title) {
     super(popupSelector);
-    this._image = image;
-    this._title = title;
+    this._image = this._popup.querySelector('.popup__images');
+		this._title = this._popup.querySelector('.popup__images-title');
+    // this._image = popupSelector.querySelector(image);
+    // this._title = popupSelector.querySelector(title);
+    // this._image = image;
+    // this._title = title;
   }
 
   open(link, name) {
